980e0e40bcc15e42fd5f84ed0655f52ebd8ed748
Suleyman GUNDUZ <suleyman.gunduz@consultant.turkcell.com.tr>
2020-11-20 00:03:00 +0300

    Serve static files on express static middleware.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const http = require('http');
 
 const app = express();
+
+app.use(express.static('build'));
+
 const server = http.createServer(app);
 
 const options = {
